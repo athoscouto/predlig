95cfcda023703674aa569f2c6395f67f3252c8e7
athoscouto <athos.couto@outlook.com>
2016-07-03 16:32:24 -0300

    Modifying script to run experiments.

--- code/fs_flickr.py
+++ code/fs_flickr.py
@@ -1,5 +1,6 @@
 from sample import *
 from evolutionary import *
+from spvlinkprediction import *
 from wrapper import *
 
 sample = CDConstructor("flickr_al.txt", 10000, (0.5, 0.5))
@@ -7,28 +8,28 @@ sample.set_classification_dataset('flickr_final_version_attributes', 'flickr_fin
 table = sample.get_classification_dataset()
 number_of_attributes = len(sample.ordered_attributes_list)
 message = "Dataset Flickr\n"
-classifiers = {"MLP": {"activation": "logistic", "hidden_layer_sizes": (4,), "learning_rate_init": 0.7}}
 metrics = ["precision", "f1", "roc_auc"]
+classifiers = (("MLP", {"activation": "logistic", "hidden_layer_sizes": (4), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (6), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (8), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (10), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (12), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (24), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (8, 10), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (16, 12), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (24, 20, 16), "learning_rate_init": 0.7}),
+               ("MLP", {"activation": "logistic", "hidden_layer_sizes": (20, 24, 16), "learning_rate_init": 0.7})
+               )
+
 for metric in metrics:
-    message += "Metric: " + metric + "\n"
-    message += "Classifier/Strategy;FFS;BaFE;BiFE\n"
-    for classifier, classifier_params in classifiers.items():
-        BiFE = BidirectionalFeatureSelection(number_of_attributes, classifier, classifier_params, metric, table[:, range(number_of_attributes)], table[:, number_of_attributes], sample.get_fold_list())
+    message = ""
+    for classifier, classifier_params in classifiers:
         BaFE = BackwardFeatureElimination(number_of_attributes, classifier, classifier_params, metric, table[:, range(number_of_attributes)], table[:, number_of_attributes], sample.get_fold_list())
         FFS = ForwardFeatureSelection(number_of_attributes, classifier, classifier_params, metric, table[:, range(number_of_attributes)], table[:, number_of_attributes], sample.get_fold_list())
+        CFS = SupervisedLinkPrediction(table, 10, classifier, classifier_params, metric)
         FFS.perform_feature_selection()
-        BiFE.perform_feature_selection()
         BaFE.perform_feature_selection()
-        message += "%s;%f/%s;%f/%s;%f/%s\n" % (classifier, FFS.best_score, len(FFS.best_var_subset), BaFE.best_score, len(BaFE.best_var_subset), BiFE.best_score, len(BiFE.best_var_subset))
-        message += "%s;%f/%s;%f/%s;%f/%s\n" % (classifier, FFS.best_score, len(FFS.best_var_subset), BaFE.best_score, len(BaFE.best_var_subset), BiFE.best_score, len(BiFE.best_var_subset))
 
-print message
-
-message = "Dataset Flickr\n"
-for metric in metrics:
-    message += "Metric: " + metric + "\n"
-    message += "Classifier/Strategy;ES1;ES2;ES3;ES4\n"
-    for classifier, classifier_params in classifiers.items():
         FS1 = EvolutionaryFeatureSelection(number_of_attributes, classifier, classifier_params, metric, table[:, range(number_of_attributes)], table[:, number_of_attributes], sample.get_fold_list(), 20, 100, 0.65, 0.05)
         FS2 = EvolutionaryFeatureSelection(number_of_attributes, classifier, classifier_params, metric, table[:, range(number_of_attributes)], table[:, number_of_attributes], sample.get_fold_list(), 20, 100, 0.65, 0.05, selection_function="tournament", selection_parameters={"tournsize": 3})
         FS3 = EvolutionaryFeatureSelection(number_of_attributes, classifier, classifier_params, metric, table[:, range(number_of_attributes)], table[:, number_of_attributes], sample.get_fold_list(), 50, 100, 0.65, 0.05, "steady_state_mode")
@@ -37,14 +38,16 @@ for metric in metrics:
         FS2.perform_feature_selection()
         FS3.perform_feature_selection()
         FS4.perform_feature_selection()
-        message += classifier
-        message += ";%s/%s" % (FS1.best_solution.fitness.values[0], [i for i in range(len(FS1.best_solution)) if FS1.best_solution[i] == 1])
-        message += ";%s/%s" % (FS2.best_solution.fitness.values[0], [i for i in range(len(FS2.best_solution)) if FS2.best_solution[i] == 1])
-        message += ";%s/%s" % (FS3.best_solution.fitness.values[0], [i for i in range(len(FS3.best_solution)) if FS3.best_solution[i] == 1])
-        message += ";%s/%s\n" % (FS4.best_solution.fitness.values[0], [i for i in range(len(FS4.best_solution)) if FS4.best_solution[i] == 1])
-        message += ";%s/%s" % (FS1.best_solution.fitness.values[0], len([i for i in range(len(FS1.best_solution)) if FS1.best_solution[i] == 1]))
-        message += ";%s/%s" % (FS2.best_solution.fitness.values[0], len([i for i in range(len(FS2.best_solution)) if FS2.best_solution[i] == 1]))
-        message += ";%s/%s" % (FS3.best_solution.fitness.values[0], len([i for i in range(len(FS3.best_solution)) if FS3.best_solution[i] == 1]))
-        message += ";%s/%s\n" % (FS4.best_solution.fitness.values[0], len([i for i in range(len(FS4.best_solution)) if FS4.best_solution[i] == 1]))
 
-print message
+	message += classifier_params["hidden_layer_sizes"]
+	message.replace(",", "_")
+	
+	message += ",{},[{}],{},[{}],{},[{}],".format(CFS.apply_classifier, 23, FFS.best_score, len(FFS.best_var_subset), BaFE.best_score, len(BaFE.best_var_subset))
+	message += "{},[{}],{},[{}],{},[{}],{},[{}];\n".format(
+							       FS1.best_solution.fitness.values[0], sum(i for i in FS1.best_solution),
+							       FS2.best_solution.fitness.values[0], sum(i for i in FS2.best_solution),
+							       FS3.best_solution.fitness.values[0], sum(i for i in FS3.best_solution),
+							       FS4.best_solution.fitness.values[0], sum(i for i in FS4.best_solution),
+						               )
+	with open("flickr.csv", "a") as res:
+		res.append(message)
